a7739e6612f55f9af83b8de6f94a5c4f1c5bde8c
Reinhard Linardi <reinhardlinardi@gmail.com>
2017-11-28 13:00:52 +0700

    Add favicon

--- index.py
+++ index.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html', carry = 'Cisco')
+    return render_template('index.html', carry = 'Roland')
 
 if __name__ == '__main__' :
     app.run(debug = True)
